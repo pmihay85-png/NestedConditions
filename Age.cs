ddb9cfbf2401135d0caad4f70641e3a6a25b8ccf
pmihay85-png <pmihay85@gmail.com>
2025-11-21 16:15:28 +0200

    Nested conditions - if, switch, case, break
    Poli

---
+++ Age.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NestedConditions
+{
+    internal class Age
+    {
+        public static bool person { get; private set; }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter your age: ");
+            person = true;
+
+            int age = int.Parse(Console.ReadLine());
+
+            if (person)
+            {
+                switch (age)
+                {
+                    case <= 12:
+                        Console.WriteLine("You are a child.");
+                        break;
+
+                    case > 12 and < 18:
+                        Console.WriteLine("You are a teenager.");
+                        break;
+
+                    case >= 18:
+                        Console.WriteLine("You are an adult.");
+                        break;
+
+                  
+                }
+            }
+        }
+    }
+}
---
+++ StudentsGrades.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Students_Grade
+{
+    internal class Students_Grade
+    {
+        static void Main()
+        {
+            Console.Write("Enter grade: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int grade))
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else
+            {
+                if (grade >= 80)
+                {
+                    Console.WriteLine("B");
+                }
+                else
+                {
+                    if (grade >= 70)
+                    {
+                        Console.WriteLine("C");
+                    }
+                    else
+                    {
+                        if (grade >= 60)
+                        {
+                            Console.WriteLine("D");
+                        }
+                        else
+                        {
+                            Console.WriteLine("F");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("That is your grade!");
+        }
+    }
+}
