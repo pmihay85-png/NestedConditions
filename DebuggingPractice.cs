e494ab2ec130e06161aa99f733b14701881c3aff
pmihay85-png <pmihay85@gmail.com>
2025-11-22 22:34:27 +0200

    DebuggingPractice
    DebuggingPractice code commit

---
+++ DebuggingPractice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DebuggingPractice
+{
+    internal class DebuggingPractice
+    {
+        static void Main(string[] args)
+
+        {
+            int score = 85;
+            if (score > 90)
+            
+            {
+                Console.WriteLine("Great job");
+            }
+          
+        }
+    }
+}
